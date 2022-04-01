c1270b5c10398dbf239b9029ca94ff584c8d6c04
Federico <boschini.federico00@gmail.com>
2022-04-01 22:50:24 +0200

    Popolamento repository

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # FastAPIExample
+
+### Comando per creare il virtual enviroment
+- python -m venv env
+
+### Comando per installare le dipendenze
+- pip install -r requirements.txt
+
+### Indirizzo URL per interfaccia Swagger
+- http://localhost:8000/docs
+
+### Indirizzo URL per interfaccia Redoc
+- http://localhost:8000/redoc
+
---
+++ main.py
@@ -0,0 +1,66 @@
+from typing import List
+from uuid import UUID, uuid4
+from fastapi import FastAPI, HTTPException
+from models import Gender, Role, User, UserUpdateRequest
+
+app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=uuid4(), 
+        first_name='Peter', 
+        last_name='Parker',
+        gender=Gender.male,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(), 
+        first_name='Michelle', 
+        last_name='Jones',
+        gender=Gender.female,
+        roles=[Role.student, Role.user]
+    ),
+]
+
+@app.get('/')
+async def root():
+    return {'Hello': 'Peter'} 
+
+@app.get('/api/v1/users')
+async def fetch_users():
+    return db
+
+@app.post('/api/v1/users')
+async def register_user(user: User):
+    db.append(user)
+    return {'id': user.id}
+
+@app.delete('/api/v1/users/{user_id}')
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"The user with id {user_id} does not exists."
+    )
+
+@app.put('/api/v1/users/{user_id}')
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"The user with id {user_id} does not exists."
+    )
+    
